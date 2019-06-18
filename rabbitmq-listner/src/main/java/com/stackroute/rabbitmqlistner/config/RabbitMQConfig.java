a50de84f0728009935da11d18e96073154566a3e
chaithra <chaitragshetty>
2019-06-18 17:39:03 +0530

    config modified

--- rabbitmq-listner/src/main/java/com/stackroute/rabbitmqlistner/config/RabbitMQConfig.java
+++ rabbitmq-listner/src/main/java/com/stackroute/rabbitmqlistner/config/RabbitMQConfig.java
@@ -6,35 +6,59 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Value("${javainuse.rabbitmq.queue}")
+    String queueName;
+
+    @Value("${javainuse.rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${javainuse.rabbitmq.routingkey}")
+    private String routingkey;
+
+    @Bean
+    Queue javaInUseQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingkey);
+    }
 //defining queue
-    private static final String MY_QUEUE ="javaqueue";
-    //creating queue
-    @Bean
-    Queue JavaInUseQueue(){
-        return new Queue(MY_QUEUE,true);
-    }
-    //Creating an Exchange
-    @Bean
-    Exchange myExchange(){
-        return ExchangeBuilder.topicExchange("javaexchange").durable(true).build();
-    }
-    //Binding Queue and Exchanges
-    @Bean
-    Binding binding(){
-        //this is the traditional way of binding
-//        return new Binding(MY_QUEUE, Binding.DestinationType.QUEUE,"myTopicExchange","topic",null);
-        //more declarative way of binding
-        return BindingBuilder
-                .bind(JavaInUseQueue())
-                .to(myExchange())
-                .with("javaroutingkey")
-                .noargs();
-    }
+//    private static final String MY_QUEUE ="javaqueue";
+//    //creating queue
+//    @Bean
+//    Queue JavaInUseQueue(){
+//        return new Queue(MY_QUEUE,true);
+//    }
+//    //Creating an Exchange
+//    @Bean
+//    Exchange myExchange(){
+//        return ExchangeBuilder.topicExchange("javaexchange").durable(true).build();
+//    }
+//    //Binding Queue and Exchanges
+//    @Bean
+//    Binding binding(){
+//        //this is the traditional way of binding
+////        return new Binding(MY_QUEUE, Binding.DestinationType.QUEUE,"myTopicExchange","topic",null);
+//        //more declarative way of binding
+//        return BindingBuilder
+//                .bind(JavaInUseQueue())
+//                .to(myExchange())
+//                .with("javaroutingkey")
+//                .noargs();
+//}
     //Creating connections to rabbitMq broker
     @Bean
     ConnectionFactory connectionFactory(){
